0db4d640bf0795ba65568909e73d0ebd1a63ba23
darkknightzh <zhanghu0722@gmail.com>
2015-03-26 09:23:36 +0800

    test
    test

---
+++ gen/com/example/testclass/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.example.testclass;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ obj/local/armeabi/objs/AndroidClass/myMeanSum.o.d
@@ -0,0 +1,70 @@
+obj/local/armeabi/objs/AndroidClass/myMeanSum.o: jni/myMeanSum.cpp \
+ jni/myMeanSum.h \
+ D:/adt-bundle-windows-x86_64-20140702/android-ndk-r10c/platforms/android-16/arch-arm/usr/include/stdio.h \
+ D:/adt-bundle-windows-x86_64-20140702/android-ndk-r10c/platforms/android-16/arch-arm/usr/include/sys/cdefs.h \
+ D:/adt-bundle-windows-x86_64-20140702/android-ndk-r10c/platforms/android-16/arch-arm/usr/include/sys/cdefs_elf.h \
+ D:/adt-bundle-windows-x86_64-20140702/android-ndk-r10c/platforms/android-16/arch-arm/usr/include/android/api-level.h \
+ D:/adt-bundle-windows-x86_64-20140702/android-ndk-r10c/platforms/android-16/arch-arm/usr/include/sys/types.h \
+ D:/adt-bundle-windows-x86_64-20140702/android-ndk-r10c/platforms/android-16/arch-arm/usr/include/stdint.h \
+ D:/adt-bundle-windows-x86_64-20140702/android-ndk-r10c/platforms/android-16/arch-arm/usr/include/sys/_types.h \
+ D:/adt-bundle-windows-x86_64-20140702/android-ndk-r10c/platforms/android-16/arch-arm/usr/include/machine/_types.h \
+ D:/adt-bundle-windows-x86_64-20140702/android-ndk-r10c/platforms/android-16/arch-arm/usr/include/linux/posix_types.h \
+ D:/adt-bundle-windows-x86_64-20140702/android-ndk-r10c/platforms/android-16/arch-arm/usr/include/linux/stddef.h \
+ D:/adt-bundle-windows-x86_64-20140702/android-ndk-r10c/platforms/android-16/arch-arm/usr/include/linux/compiler.h \
+ D:/adt-bundle-windows-x86_64-20140702/android-ndk-r10c/platforms/android-16/arch-arm/usr/include/asm/posix_types.h \
+ D:/adt-bundle-windows-x86_64-20140702/android-ndk-r10c/platforms/android-16/arch-arm/usr/include/asm/types.h \
+ D:/adt-bundle-windows-x86_64-20140702/android-ndk-r10c/platforms/android-16/arch-arm/usr/include/linux/types.h \
+ D:/adt-bundle-windows-x86_64-20140702/android-ndk-r10c/platforms/android-16/arch-arm/usr/include/machine/kernel.h \
+ D:/adt-bundle-windows-x86_64-20140702/android-ndk-r10c/platforms/android-16/arch-arm/usr/include/sys/sysmacros.h \
+ D:/adt-bundle-windows-x86_64-20140702/android-ndk-r10c/platforms/android-16/arch-arm/usr/include/stdlib.h \
+ D:/adt-bundle-windows-x86_64-20140702/android-ndk-r10c/platforms/android-16/arch-arm/usr/include/string.h \
+ D:/adt-bundle-windows-x86_64-20140702/android-ndk-r10c/platforms/android-16/arch-arm/usr/include/malloc.h \
+ D:/adt-bundle-windows-x86_64-20140702/android-ndk-r10c/platforms/android-16/arch-arm/usr/include/alloca.h \
+ D:/adt-bundle-windows-x86_64-20140702/android-ndk-r10c/platforms/android-16/arch-arm/usr/include/strings.h \
+ D:/adt-bundle-windows-x86_64-20140702/android-ndk-r10c/platforms/android-16/arch-arm/usr/include/memory.h
+
+jni/myMeanSum.h:
+
+D:/adt-bundle-windows-x86_64-20140702/android-ndk-r10c/platforms/android-16/arch-arm/usr/include/stdio.h:
+
+D:/adt-bundle-windows-x86_64-20140702/android-ndk-r10c/platforms/android-16/arch-arm/usr/include/sys/cdefs.h:
+
+D:/adt-bundle-windows-x86_64-20140702/android-ndk-r10c/platforms/android-16/arch-arm/usr/include/sys/cdefs_elf.h:
+
+D:/adt-bundle-windows-x86_64-20140702/android-ndk-r10c/platforms/android-16/arch-arm/usr/include/android/api-level.h:
+
+D:/adt-bundle-windows-x86_64-20140702/android-ndk-r10c/platforms/android-16/arch-arm/usr/include/sys/types.h:
+
+D:/adt-bundle-windows-x86_64-20140702/android-ndk-r10c/platforms/android-16/arch-arm/usr/include/stdint.h:
+
+D:/adt-bundle-windows-x86_64-20140702/android-ndk-r10c/platforms/android-16/arch-arm/usr/include/sys/_types.h:
+
+D:/adt-bundle-windows-x86_64-20140702/android-ndk-r10c/platforms/android-16/arch-arm/usr/include/machine/_types.h:
+
+D:/adt-bundle-windows-x86_64-20140702/android-ndk-r10c/platforms/android-16/arch-arm/usr/include/linux/posix_types.h:
+
+D:/adt-bundle-windows-x86_64-20140702/android-ndk-r10c/platforms/android-16/arch-arm/usr/include/linux/stddef.h:
+
+D:/adt-bundle-windows-x86_64-20140702/android-ndk-r10c/platforms/android-16/arch-arm/usr/include/linux/compiler.h:
+
+D:/adt-bundle-windows-x86_64-20140702/android-ndk-r10c/platforms/android-16/arch-arm/usr/include/asm/posix_types.h:
+
+D:/adt-bundle-windows-x86_64-20140702/android-ndk-r10c/platforms/android-16/arch-arm/usr/include/asm/types.h:
+
+D:/adt-bundle-windows-x86_64-20140702/android-ndk-r10c/platforms/android-16/arch-arm/usr/include/linux/types.h:
+
+D:/adt-bundle-windows-x86_64-20140702/android-ndk-r10c/platforms/android-16/arch-arm/usr/include/machine/kernel.h:
+
+D:/adt-bundle-windows-x86_64-20140702/android-ndk-r10c/platforms/android-16/arch-arm/usr/include/sys/sysmacros.h:
+
+D:/adt-bundle-windows-x86_64-20140702/android-ndk-r10c/platforms/android-16/arch-arm/usr/include/stdlib.h:
+
+D:/adt-bundle-windows-x86_64-20140702/android-ndk-r10c/platforms/android-16/arch-arm/usr/include/string.h:
+
+D:/adt-bundle-windows-x86_64-20140702/android-ndk-r10c/platforms/android-16/arch-arm/usr/include/malloc.h:
+
+D:/adt-bundle-windows-x86_64-20140702/android-ndk-r10c/platforms/android-16/arch-arm/usr/include/alloca.h:
+
+D:/adt-bundle-windows-x86_64-20140702/android-ndk-r10c/platforms/android-16/arch-arm/usr/include/strings.h:
+
+D:/adt-bundle-windows-x86_64-20140702/android-ndk-r10c/platforms/android-16/arch-arm/usr/include/memory.h:
